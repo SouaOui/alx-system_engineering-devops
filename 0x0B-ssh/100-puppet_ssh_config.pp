837b948faacfcfe90636d34984c7d3b8f3499e64
SouaOui <soumiaouzat@gmail.com>
2023-09-25 02:29:21 +0100

    Just Typing Unfocused........

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Client configuration file (w/ Puppet)
 # ssh configuration .pp
 file_line { 'give the Identity file':
-    ensure                 => 'present'
-    line                   => '  IdentityFile ~/.ssh/school',
-    path                   => '/etc/ssh/ssh_config',
+    ensure => 'present'
+    line   => '  IdentityFile ~/.ssh/school',
+    path   => '/etc/ssh/ssh_config',
 }
 file_line { 'no password authentification':
-    ensure                 => 'present'
-    line                   => '  PasswordAuthentication no',
-    path                   => '/etc/ssh/ssh_config',
+    ensure => 'present'
+    line   => '  PasswordAuthentication no',
+    path   => '/etc/ssh/ssh_config',
 }
