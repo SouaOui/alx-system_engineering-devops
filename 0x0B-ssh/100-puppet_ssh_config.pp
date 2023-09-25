ecd0aadffb62400573c98e23256008629b398954
SouaOui <soumiaouzat@gmail.com>
2023-09-25 02:26:54 +0100

    Just Typing...;;;;;

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,10 @@
 file_line { 'give the Identity file':
     ensure                 => 'present'
     line                   => '  IdentityFile ~/.ssh/school',
-    path                   => '/etc/ssh/ssh_config.'
+    path                   => '/etc/ssh/ssh_config',
 }
 file_line { 'no password authentification':
     ensure                 => 'present'
     line                   => '  PasswordAuthentication no',
-    path                   => '/etc/ssh/ssh_config.'
+    path                   => '/etc/ssh/ssh_config',
 }
