bd6a4b3c7d1f81e3b6bb86c6a0973c17e0211bac
SouaOui <soumiaouzat@gmail.com>
2023-09-25 02:26:06 +0100

    Effect of Being sleepy

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 # ssh configuration .pp
 file_line { 'give the Identity file':
     ensure                 => 'present'
-    line                   => 'IdentityFile ~/.ssh/school',
+    line                   => '  IdentityFile ~/.ssh/school',
     path                   => '/etc/ssh/ssh_config.'
 }
 file_line { 'no password authentification':
     ensure                 => 'present'
-    line                   => 'PasswordAuthentication no',
+    line                   => '  PasswordAuthentication no',
     path                   => '/etc/ssh/ssh_config.'
 }
