caf4f90781ff89de348052c181d321a004ef242e
SouaOui <soumiaouzat@gmail.com>
2023-09-25 02:23:59 +0100

    add configuration to etc/ssh/ssh_config using puppet syntax

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Client configuration file (w/ Puppet)
 # ssh configuration .pp
-file { 'give the Identity file':
+file_line { 'give the Identity file':
     ensure                 => 'present'
     passwordauthentication => 'no',
     line                   => 'IdentityFile ~/.ssh/school',
     path                   => '/etc/ssh/ssh_config.'
 }
-file { 'no password authentification':
+file_line { 'no password authentification':
     ensure                 => 'present'
     passwordauthentication => 'no',
     line                   => 'PasswordAuthentication no',
