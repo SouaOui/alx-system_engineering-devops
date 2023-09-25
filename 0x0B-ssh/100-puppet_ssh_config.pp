59dc02156e95d6d44a86355cc0796f78a9af0b7a
SouaOui <soumiaouzat@gmail.com>
2023-09-25 06:51:48 +0100

    Update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,13 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-    ensure => 'present'
-    path   => '/etc/ssh/ssh_config',
-    line   => '  PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 file_line { 'Delare identity file':
-    ensure => 'present'
-    path   => '/etc/ssh/ssh_config',
-    line   => '  IdentityFile ~/.ssh/school',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
+
