7e8886d6a3c3cf3e30d21b9821202d294ecfd450
SouaOui <soumiaouzat@gmail.com>
2023-11-03 18:20:43 +0100

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""A script that returns todos of employees bt id"""
+"""fetching user data using api"""
 
-import json
+
+import requests as req
 import sys
-import urllib.request
-
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
-    userId = sys.argv[1]
+    ID_Employee = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(ID_Employee)
+    url_to_do = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
+        ID_Employee)
+    response = req.get(url).json()
+    name = response.get('name')
+    response_to_dos = req.get(url_to_do).json()
 
-    req = urllib.request.Request(f"{url}/users/{userId}")
-    with urllib.request.urlopen(req) as response:
-        html = response.read().decode('utf-8')
-        to_json = json.loads(html)
-
-    with urllib.request.urlopen(f"{url}/todos?userId={userId}") as response:
-        html2 = response.read().decode('utf-8')
-        todo_json = json.loads(html2)
-
-        count = 0
-        check_list = ""
-        for todo in todo_json:
-            task_done = todo['completed']
-            if task_done is True:
-                count = count + 1
-                check_list += "\t" + todo['title'] + "\n"
-
-    name = to_json['name']
-    total = len(todo_json)
-    print(
-        "Employee {} is done with tasks({}/{}):"
-        .format(name, count, total))
-    print(check_list)
+    count = 0
+    total = len(response_to_dos)
+    for to_do in response_to_dos:
+        if to_do.get('completed'):
+            count += 1
+    print("Employee {} is done with tasks({}/{}):".format(name, count, total))
+    for to_do in response_to_dos:
+        if to_do.get('completed'):
+            print("     {}".format(to_do['title']))
