2e7afbdff733399a5cdbf3990d5e5c99e3831cde
SouaOui <soumiaouzat@gmail.com>
2023-11-14 11:14:38 +0100

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+List the TOP 10 of popular in Reddit
+"""
+
+import requests as req
+import sys
+
+headers = {"User-Agent": "User_Agent_/3.0"}
+
+
+def top_ten(subreddit):
+    """Method Subscribers"""
+    subreddit = sys.argv[1]
+    url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    response = req.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for query in data['data']['children']:
+            print(query['data']['title'])
+    return None
