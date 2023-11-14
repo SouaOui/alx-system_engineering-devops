dffb69d272dd55abdcdf9cc7c5044a1209af0628
SouaOui <soumiaouzat@gmail.com>
2023-11-14 11:20:50 +0100

    1.Top Ten__

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,13 @@ List the TOP 10 of popular in Reddit
 """
 
 import requests as req
-import sys
+from sys import argv
 headers = {"User-Agent": "User_Agent_/3.0"}
 
 
 def top_ten(subreddit):
     """Top Ten popular"""
-    subreddit = sys.argv[1]
+    subreddit = argv[1]
     url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
     response = req.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
